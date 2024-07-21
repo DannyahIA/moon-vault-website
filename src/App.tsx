8bad8d07ac3e74b0ea401fc05fd6fb0548cd5498
DannyahIA <danieltavares.mata@gmail.com>
2024-07-21 16:14:11 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Login from './pages/login/Login';
+import MainPage from './pages/main_page/MainPage';
+import Hamburguer from './pages/hamburguer/Hamburguer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+        <div className="content-wrapper">
+            <div className="sidebar">
+                <Hamburguer />
+            </div>
+            <div className="main-content">
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </div>
     </div>
+</Router>
   );
 }
 
---
+++ src/pages/main_page/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPage.css';
+
+const MainPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default MainPage;
